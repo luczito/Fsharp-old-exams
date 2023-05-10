59d5e79172d739dee80c9145cab47dc23b7bac36
LF <lucx1306@gmail.com>
2023-05-10 14:19:25 +0200

    init

---
+++ exam-2019-05/exam-2019-05/Program.fs
@@ -0,0 +1,14 @@
+﻿type Peano =
+    | O
+    | S of Peano
+
+let rec toInt (n: Peano) : uint32 =
+    match n with
+    | O -> 0u
+    | S p -> 1u + toInt p
+
+let rec fromInt (x: uint32) : Peano =
+    match x with
+    | 0u -> O
+    | _ -> S (fromInt (x - 1u))
+
